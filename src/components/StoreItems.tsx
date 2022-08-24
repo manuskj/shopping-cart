a7cc532849ba592794059de013701948acf8cdf7
Manuela Santos <manuelass.biz@gmail.com>
2022-08-23 22:33:56 -0300

    [feat] import bootstrap feature

--- src/components/StoreItems.tsx
+++ src/components/StoreItems.tsx
@@ -1,4 +1,4 @@
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import { formatCurrency } from "../utilities/formatCurrency";
 
